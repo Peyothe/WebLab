4b9a23758d03b70ceb6b98f66fcdf4e4c4c1380b
Peyothe <peyothe@hotmail.com>
2018-09-20 10:35:57 +0200

    First

--- init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('Ready w/ JQ');
-});
-
-$(document).ready(function() {
-    $('#menu a').on('click', function(e) {
-        e.preventDefault();
-    })
-});
